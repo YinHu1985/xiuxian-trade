044ab9004393515a67ede2cc795c151b921abc58
Yin Hu <yinhu@Yins-MacBook-Pro.local>
2026-07-20 15:57:52 +0800

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/xiuxian-trade/',  // ← 改成你的仓库名，前后斜杠都要
   build: {
     sourcemap: 'hidden',
   },
